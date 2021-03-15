b5859a4cae9e6355383d79b782e6e5b8e7ede6ec
Production server <nitorac.r@gmail.com>
2021-03-15 23:03:42 +0100

    Download script v1

---
+++ downloadModel.sh
@@ -0,0 +1,27 @@
+echo "Téléchargement de 11 fichiers"
+echo "Téléchargement 1 / 11 ..."
+gdown https://drive.google.com/uc?id=1Fe3C3tUgH5BktZ9R6hPlismAy-YRYQTS || exit 1 # aa
+echo "Téléchargement 2 / 11 ..."
+gdown https://drive.google.com/uc?id=1HOxruE0sOGgSblfBcxkMmc3xYM3hfAEu || exit 1 # ab
+echo "Téléchargement 3 / 11 ..."
+gdown https://drive.google.com/uc?id=1Qcjy7kRQSypE5pUylc5UmM8zj8bcfXfP || exit 1 # ac
+echo "Téléchargement 4 / 11 ..."
+gdown https://drive.google.com/uc?id=1UmsqjfdiKCnl_1AXnwC01k1WvaNK2Aea || exit 1 # ad
+echo "Téléchargement 5 / 11 ..."
+gdown https://drive.google.com/uc?id=18cYaHczpKRpuy08iDXl55Me1yCvwf3P_ || exit 1 # ae
+echo "Téléchargement 6 / 11 ..."
+gdown https://drive.google.com/uc?id=1MDSBxypSnlMd1n2WWNiiEzgCbJMtCC_w || exit 1 # af
+echo "Téléchargement 7 / 11 ..."
+gdown https://drive.google.com/uc?id=1zQX_9B7VHFAPdXgllW0vr2YHw4eydjHC || exit 1 # ag
+echo "Téléchargement 8 / 11 ..."
+gdown https://drive.google.com/uc?id=1zs3mO_9idarACqJJecVwzVU5GposQ9XN || exit 1 # ah 
+echo "Téléchargement 9 / 11 ..."
+gdown https://drive.google.com/uc?id=1ASoAf7nca1arPqewKMpxwQ2vWpL80eUL || exit 1 # ai
+echo "Téléchargement 10 / 11 ..."
+gdown https://drive.google.com/uc?id=1IMCKosRDa5_jMqxrd2BSrSdssLWFgMLj || exit 1 # aj
+echo "Téléchargement 11 / 11 ..."
+gdown https://drive.google.com/uc?id=1hJ3ulT2q2mYdefjNmmMzsX96FlzC8Eh3 || exit 1 # ak
+echo ""
+echo ""
+echo "Téléchargements terminés"
+exit 0
